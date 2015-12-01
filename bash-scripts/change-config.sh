a6f86b4268ddf16a3a02d0dfbc687a073011bf0e
oshirohugo <oshiro.hugo@gmail.com>
2015-12-01 14:44:25 -0200

    Add script to change keys in conf files

---
+++ bash-scripts/change-config.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Usage: change-config.sh key value conf-file
+#
+# Autor: oshirohugo
+
+if [ "$#" -ne 3 ];then
+    echo "Usage:"
+    printf "\t%s <key> <value> <conf-file>\n" $0
+    exit 1
+fi
+
+TARGET_KEY=$1
+REPLACEMENT_VALUE=$2
+CONFIG_FILE=$3
+
+sed -i "s/\($TARGET_KEY *= *\).*/\1$REPLACEMENT_VALUE/" $CONFIG_FILE
