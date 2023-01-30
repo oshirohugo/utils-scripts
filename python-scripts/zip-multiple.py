1cf0d2dae999cc95ce7bd2a2794e0b912e95aa13
Hugo Oshiro <oshiro.hugo@gmail.com>
2023-01-30 20:19:49 +0100

    adjust for numeric chapters only

--- python-scripts/zip-multiple.py
+++ python-scripts/zip-multiple.py
@@ -4,19 +4,12 @@ import os
 import shutil
 import re
 
-# base = '/home/hoshiro/Documents/Mangas/Blade of the Immortal'
-base_dir = '/home/hoshiro/Documents/Mangas/Blade of the Immortal'
-# base_dir = '/home/hoshiro/Documents/Mangas/Test Manga'
+base_dir = '/home/hoshiro/Documents/Mangas/The Promised Neverland'
 
-dirs = os.listdir(base_dir)
+dirs = sorted(os.listdir(base_dir))
 
 count = 0
-max_chapter = -1
 for dir in dirs:
-    m = re.search('Chapter\s(\d+)', dir)
-    chapter = int(m.group(1))
-    max_chapter = max(max_chapter, chapter)
-
     files = os.listdir(os.path.join(base_dir, dir))
     count += len(files)
 
@@ -32,8 +25,8 @@ os.mkdir(tmp_dir)
 print('Renaming files')
 
 page_counter = 1
-for chapter in range(1, max_chapter + 1):
-    dir_path = os.path.join(base_dir, f'Chapter {chapter}')
+for dir in dirs:
+    dir_path = os.path.join(base_dir, dir)
     files = sorted(os.listdir(dir_path))
 
     for file in files:
